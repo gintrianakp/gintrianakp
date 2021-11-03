3e73a2722cc290d73bd786efa880fc54dcb02fb7
gintrianakp <93664676+gintrianakp@users.noreply.github.com>
2021-11-03 21:51:26 +0700

    Update infobirama.js

--- infobirama.js
+++ infobirama.js
@@ -1,4 +1,3 @@
-function Bootstrap (info){
 document.querySelector(".third").addEventListener('click', function(){
 var gagal="Kami ucapkan dengan berat hati <br><b style='color: red;'>MOHON MAAF ANDA TIDAK TERPILIH</b><br> menjadi bagian dari <br> <b>PERS BIRAMA UNIKOM</b>. <br><br> <p style='text-align: justify;'>Semoga kalian tetap belajar jurnalistik dimanapun yaa,tetap semangat!kami tunggu di lain kesempatan! Kami selaku panitia sangat berterima kasih kepada kalian karena sudah berpartisipasi dan bekerja sama dalam proses magang ini.</p>"
 var lolos="Kami ucapkan dengan senang hati <br><b style='color: green;'>SELAMAT ANDA TERPILIH</b><br> menjadi bagian dari <br> <b>PERS BIRAMA UNIKOM</b>  <br><br> <p style='text-align: justify;'>Semoga yang terpilih amanah,dapat menjalankan kewajiban dengan baik,dan mengikuti segala kegiatan di pers birama. Kami selaku panitia sangat berterima kasih kepada kalian karena sudah berpartisipasi dan bekerja sama dalam proses magang ini.</p>";
@@ -22,4 +21,3 @@ if(info==0){
   });
 }
 });
-}
